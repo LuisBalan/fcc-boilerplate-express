8503f937f9281030edcde0aafcc21507364d61a7
LuisBalan <89665850+LuisBalan@users.noreply.github.com>
2021-11-19 20:40:00 +0000

    "make Hello Express response in the server"

--- myApp.js
+++ myApp.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
+var port = 8080
 
 
-console.log('Hello World')
+const newRequest = function(req, res){
+  res.send("\'Hello Express\'")
+};
 
 
+app.get('/', newRequest)
+
+app.listen(port, () => {
+  console.log('ready!')
+})
 
 module.exports = app;
