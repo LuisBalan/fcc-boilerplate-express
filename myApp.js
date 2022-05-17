1dd81ffc93dc724995d4d870f2b6917a98e1f23e
LuisBalan <jlbalcz@gmail.com>
2022-05-17 02:46:36 -0500

    sending just JSON response without a file

--- myApp.js
+++ myApp.js
@@ -6,13 +6,14 @@ let app = express();
 //     res.send("Hello Express");
 // })
 
-app.use('/public', express.static(__dirname + '/public'));
+// app.use('/public', express.static(__dirname + '/public'));
 
 //Serving an html file from server
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/views/index.html');
+// });
 
+//Serving a JSON response on an specific route
 app.get('/json', (req, res) => {
     res.json({message: "Hello json"});
 });
