30292107e97c3f9d8b47b9b6bccd427ac9c0d2a1
LuisBalan <jlbalcz@gmail.com>
2022-05-17 01:08:59 -0500

    first exercise

--- myApp.js
+++ myApp.js
@@ -1,11 +1,4 @@
 var express = require('express');
 var app = express();
-var port = 8080;
 
-app.listen(port, () => {
-    
-    console.log('Hello World!')
-})
-
-
-module.exports = app;
+console.log("Hello World")
