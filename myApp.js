307aea5295afe2b66d75b215c6fbab012ec19e0a
LuisBalan <jlbalcz@gmail.com>
2022-05-17 02:13:45 -0500

    send file html from server

--- myApp.js
+++ myApp.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
+// Serve a Hello Express
+// app.get("/", (req, res) => {
+//     res.send("Hello Express");
+// })
 
-app.get("/", (req, res) => {
-    res.send("Hello Express");
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
 })
 
 module.exports = app;
