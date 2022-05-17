17b87bfff002d7a1aed4e0bcf1150d8348366dea
LuisBalan <jlbalcz@gmail.com>
2022-05-17 17:29:49 -0500

    add solution to implement middlewares from root

--- myApp.js
+++ myApp.js
@@ -15,19 +15,34 @@ let app = express();
 // });
 
 //Serving a JSON response on an specific route
-app.get('/json', (req, res) => {
-    let responseValue;
-    let stringResponse = "Hello json"
-    if(process.env.MESSAGE_STYLE == "uppercase"){
-        responseValue = stringResponse.toUpperCase();
-    }else{
-        responseValue = stringResponse;
-    };
-    res.json({message: responseValue});
+
+// app.get('/json', (req, res) => {
+//     let responseValue;
+//     let stringResponse = "Hello json"
+//     if(process.env.MESSAGE_STYLE == "uppercase"){
+//         responseValue = stringResponse.toUpperCase();
+//     }else{
+//         responseValue = stringResponse;
+//     };
+//     res.json({message: responseValue});
+// });
+
+//Implement a Root-Level Request Logger Middleware
+
+// function gralMiddleware(arg1, arg2, callback1){
+//     console.log(`${arg1.method} ${arg1.path} ${arg1.ip}`);
+//     callback1;
+// };
+
+// app.use((req, res) => {
+//     console.log(`${req.method} ${req.path} ${req.ip}`);
+//     next();
+// });
+
+app.use((req, res) => {
+    console.log(`${req.method} ${req.path} ${req.ip}`)
+    next();
 });
-let a = "string";
-
-
 
 
 
