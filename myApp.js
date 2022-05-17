752aa56a2712554105bc12d0d3310b1629c20d32
LuisBalan <jlbalcz@gmail.com>
2022-05-17 01:58:59 -0500

    setting file to server

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
-var port = 8080
+
 
 app.get("/", (req, res) => {
-    res.send("Hello Express")
+    res.send("Hello Express");
 })
+
+module.exports = app;
